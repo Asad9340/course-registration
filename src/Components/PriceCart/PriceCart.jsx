cced109eec0490bef431e25883c6bd05e610c1cf
Asad9340 <asadulimran1999@gmail.com>
2024-03-14 01:03:02 +0600

    added total credit hour in price section

--- src/Components/PriceCart/PriceCart.jsx
+++ src/Components/PriceCart/PriceCart.jsx
@@ -6,12 +6,22 @@ function PriceCat({ selectedCourse }) {
       </h3>
       <div>
         <h3 className="text-xl font-bold text-[#1C1B1B]">Course Name: </h3>
-        <p className="text-black mt-3">{
-          selectedCourse.map((course, index) => <li className="list-decimal mb-1" key={index}>{course.name}</li>)
-        }</p>
+        <p className="text-black mt-3">
+          {selectedCourse.map((course, index) => (
+            <li className="list-decimal mb-1 text-[#1C1B1B99]" key={index}>
+              {course.name}
+            </li>
+          ))}
+        </p>
+      </div>
+      <div>
+        <h3 className="text-base font-medium text-[#1C1B1BCC]">
+          Total Credit Hour :{' '}
+          {selectedCourse.reduce((p, c) =>p + c.credit,0)} hour
+        </h3>
       </div>
     </div>
   );
 }
 
-export default PriceCat
+export default PriceCat;
