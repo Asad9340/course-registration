d4a265eb911e4c8edbc6b60141fb532f05a6c008
Asad9340 <asadulimran1999@gmail.com>
2024-03-14 02:29:11 +0600

    fixes some minor bugs and add hover effect and selected background

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -1,9 +1,20 @@
 import { PiCurrencyCircleDollarFill } from 'react-icons/pi';
 import { FaBookOpen } from 'react-icons/fa';
+import { useState } from 'react';
 function Course({ course, handleSelectBtn }) {
   const { photo, name, description, price, credit } = course;
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
+  const handleBtn = () => {
+    handleSelectBtn(course),
+      handleClick();
+  }
   return (
-    <div className=" rounded-lg border py-4 px-2 bg-[#ffffff]">
+    <div
+      className={` rounded-lg border p-2 bg-[#ffffff] hover:bg-gray-300 duration-300         ${click?'bg-gray-200':''}`}
+    >
       <img src={photo} alt="" className=" w-full rounded-t-md object-cover" />
       <div className="p-4">
         <h1 className="inline-flex items-center text-lg font-semibold">
@@ -21,7 +32,7 @@ function Course({ course, handleSelectBtn }) {
           </div>
         </div>
         <button
-          onClick={() => handleSelectBtn(course)}
+          onClick={handleBtn}
           type="button"
           className="mt-4 w-full rounded-md bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
         >
