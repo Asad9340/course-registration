68ffa988a768dd64758f4c008b9ba11e67a66944
Asad9340 <asadulimran1999@gmail.com>
2024-03-29 21:45:51 +0600

    added some padding on heading

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ export const totalCourse = 15;
 function App() {
   const [selectedCourse, setSelectedCourse] = useState([]);
   const handleSelectBtn = course => {
-    const alreadyExist = selectedCourse.find(
-      element => element.id === course.id
-    );
     const totalCredit = selectedCourse.reduce((p, c) => p + c.credit, 0);
     if (totalCredit + course.credit > totalCourse) {
       return toast.warn(`Only ${totalCourse} credit  is allowed`);
     }
+
+    const alreadyExist = selectedCourse.find(
+      element => element.id === course.id
+    );
+
     if (!alreadyExist) {
       toast.success(`Course "${course.name}" added successfully`);
       const allCourse = [...selectedCourse, course];
@@ -25,7 +27,7 @@ function App() {
   };
   return (
     <div>
-      <h1 className="text-2xl md:text-4xl lg:text-5xl font-extrabold text-center text-white bg-gray-900 py-4 mt-10">
+      <h1 className="text-2xl md:text-4xl lg:text-5xl font-extrabold text-center text-white bg-gray-900 py-5 mt-10">
         Course Registration
       </h1>
       <div className="flex flex-col md:grid gap-4 grid-cols-12  mt-8 md:mt-12 mx-2 md:mx-4">
